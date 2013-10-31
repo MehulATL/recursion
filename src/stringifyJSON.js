c6a9f5dde62a73d4303faa4d7fe9b64380327cf3
Mehul Patel <mehul8@gmail.com>
2013-10-31 18:55:39 -0400

    stringifyJSON.js is not quite passing everything yet. Almost there.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,34 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+  if(typeof(obj) === 'string'){
+  	return '"' + obj + '"';
+  }
+
+  if((typeof(obj) === 'number') || (typeof(obj) === 'boolean') || (typeof(obj) === null)){
+  	return = String(obj);
+  }
+
+  if((typeof(obj) === undefined) || (typeof(obj) === 'function')){
+    return = '';
+  }
+
+  if(Array.isArray(obj)){
+    var resultingArray = [];
+    for (var i = 0; i < obj.length; i++){
+      resultingArray.push(stringifyJSON(obj[i]));
+    });
+    return '[' + resultingArray.join(',') + ']';
+  }
+
+  if(Object.prototype.toString.call(obj) === "[object Object]"){
+    var resultingArray = [];
+    for (var key in obj){
+      if((typeof(obj[key]) === undefined) || (typeof(obj[key]) === 'function')){
+        return = '{}';
+      }else{
+        resultingArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + resultingArray.join(',') + '}';
+  }
 };
