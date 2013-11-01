4bf58fcdf7dd3fb70a190b962bdde58524bd31be
Mehul Patel <mehul8@gmail.com>
2013-11-01 12:07:38 -0400

    think I'm almost done, just gotta deal with a TypeError

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,21 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+  var elements = [];
+  var currentNodes = document.body.childNodes;
+
+  var findElements = function(nodes){
+  	for (var i = 0; i < nodes.length; i++){
+  	  var node = nodes[i];
+  	  if (node.classList.contains(className)){
+  	  	elements.push(node);
+  	  }
+  	  if (node.hasChildNodes()){
+  	  	findElements(node.childNodes);
+  	  }
+  	}
+  };
+
+  findElements(currentNodes);
+  return elements;
 };
